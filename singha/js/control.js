b680d4c7cc2be2d5b1216457bf1ab7816331a6b8
daynomaddev <banyapon@daydev.com>
2024-02-15 23:11:55 +0700

    first commit

---
+++ singha/js/control.js
@@ -0,0 +1,24 @@
+var alphabet_J_online = sessionStorage.setItem('J_online', "0");
+var alphabet_J_active = sessionStorage.setItem('J_active', "0");
+var alphabet_J_after = sessionStorage.setItem('J_after', "0");
+
+var alphabet_E_online = sessionStorage.setItem('E_online', "0");
+var alphabet_E_active = sessionStorage.setItem('E_active', "0");
+var alphabet_E_after = sessionStorage.setItem('E_after', "0");
+
+
+var alphabet_A_online = sessionStorage.setItem('A_online', "0");
+var alphabet_A_active = sessionStorage.setItem('A_active', "0");
+var alphabet_A_after = sessionStorage.setItem('A_after', "0");
+
+var alphabet_T_online = sessionStorage.setItem('T_online', "0");
+var alphabet_T_active = sessionStorage.setItem('T_active', "0");
+var alphabet_T_after = sessionStorage.setItem('T_after', "0");
+
+var alphabet_H_online = sessionStorage.setItem('H_online', "0");
+var alphabet_H_online = sessionStorage.setItem('H_active', "0");
+var alphabet_H_online = sessionStorage.setItem('H_after', "0");
+
+
+
+// Saves to sessionStorage part of web storage
